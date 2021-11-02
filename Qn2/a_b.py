748e80cce0de15c506218c7a88d5c054857c39b9
HotMonkeyWings <devsony52@gmail.com>
2021-11-02 18:55:51 +0530

    Updated report, renamed Qn2 programs

---
+++ Qn2/a_b.py
@@ -0,0 +1,105 @@
+import numpy as np
+from math import pi, log
+
+# Discriminant function as defined in the question
+def discriminant_function(x, mean, cov, d, P):
+    # Checking if the dimensions turn out to be scalars in the case only 1 feature is being taken.
+    if d == 1:
+        output = -0.5*(x - mean) * (1/cov)
+        output = output * (x - mean)
+        output += -0.5*d*log(2*pi) - 0.5*log(cov) 
+
+    else: 
+        output = np.matmul(-0.5*(x - mean), np.linalg.inv(cov))
+        output = np.matmul(output, (x - mean).T)
+        output += -0.5*d*log(2*pi) - 0.5*log(np.linalg.det(cov)) 
+    
+    # Adding Prior Probability
+    output += (log(P) if P != 0 else 0)
+
+    return output
+        
+def main():
+
+    # Sample Data
+    data = [
+        # W1
+        np.array([
+            [-5.01, -8.12, -3.68],
+            [-5.43, -3.48, -3.54],
+            [1.08, -5.52, 1.66],
+            [0.86, -3.78, -4.11],
+            [-2.67, 0.63, 7.39],
+            [4.94, 3.29, 2.08],
+            [-2.51, 2.09, -2.59],
+            [-2.25, -2.13, -6.94],
+            [5.56, 2.86, -2.26],
+            [1.03, -3.33, 4.33]
+        ]),
+
+        # W2
+        np.array([
+            [-0.91, -0.18, -0.05],
+            [1.30, -2.06, -3.53],
+            [-7.75, -4.54, -0.95],
+            [-5.47, 0.50, 3.92],
+            [6.14, 5.72, -4.85],
+            [3.60, 1.26, 4.36],
+            [5.37, -4.63, -3.65],
+            [7.18, 1.46, -6.66],
+            [-7.39, 1.17, 6.30],
+            [-7.50, -6.32, -0.31]
+
+        ]),
+
+        # W3
+        np.array([
+            [5.35, 2.26, 8.13],
+            [5.12, 3.22, -2.66],
+            [-1.34, -5.31, -9.87],
+            [4.48, 3.42, 5.19],
+            [7.11, 2.39, 9.21],
+            [7.17, 4.33, -0.98],
+            [5.75, 3.97, 6.65],
+            [0.77, 0.27, 2.41],
+            [0.90, -0.43, -8.71],
+            [3.52, -0.36, 6.43]
+        ]) 
+    ]
+
+    # Measure mean and covariance
+    means = []
+    cov = []
+    for i in range(len(data)):
+        means.append(data[i].mean(axis=0))
+        cov.append(np.cov(data[i].T))
+    means, cov = np.array(means), np.array(cov)
+
+    # Configuration values
+    n = len(data) - 1
+    P = [0.5, 0.5, 0]
+    d = 1
+
+    # Taking each dataset from the classes in sample data
+    for j in range(n):
+        print("\nData classes should be classified as:", j+1)
+        total_count, count = 0, 0
+
+        # Taking x as dataset belonging to class j + 1
+        for x in data[j]:
+            g_values = [0 for g in range(n)]        # Array for all discrminant function outputs.
+
+            # Itering through each class' discriminant function
+            for i in range(n):
+                g_values[i] = discriminant_function(x[0], means[i][0], cov[i][0][0], d, P[i])
+
+            # Now to output the maximum result 
+            result = g_values.index(max(g_values)) + 1
+            print(x, "\twas classified as", result)
+            total_count, count = total_count + 1, (count + 1 if j == result - 1 else count)
+        
+        print("Success Rate:", (count/total_count)*100,"%")
+        print("Fail Rate:", 100 - ((count/total_count))*100,"%")
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Based on the formula given:
 The function has been defined:
 ![DF](Qn1/DF.png)
 
-The variables can be configured based on the scenario: 
+The variables can be configured based on the scenario. Here, it's assumed that prior probabilities are equally distributed and all features are taken: 
 ![Vars](Qn1/Vars.png)
 
 The input is the sample dataset, each set separated by the class they belong to as given below:
@@ -23,11 +23,29 @@ The input is the sample dataset, each set separated by the class they belong to 
 
 In order to classify the sample data, we first run the function through our sample dataset, classwise. On each sample, we find the class which gives the maximum output from its discriminant function. 
 
-A count and total count is maintained in order to find the success rates.
+A count and total count is maintained in order to find the success and failiure rates.
 
 ![Main](Qn1/main.png)
 
 Assuming that all classes have an equal prior probability (as per the configuration in the example picture), the following output is produced:
 
 ![Output](Qn1/Output.png)
+
+## Solution 2
+
+### Part (a) and (b)
+In order to match the question, the configuration variables are altered. 
+
+- (data-1) for n indicates that only 2 classes will be considered (the final class would not be considered as its Prior probability is 0, implying that it wouldn't appear.)
+- The d value is changed to 1, indicating that only 1 feature will be used. (which is x<sub>1</sub>)
+
+![NewVars](Qn2/Vars.png)
+
+The parameters being passed is also changed
+
+- x\[0] indicates that only x^1 will be used.
+- means\[i]\[0] indiciates that we need the mean only for x<sub>1</sub>).
+- cov\[i]\[0]\[0] indicates the variance of feature x<sub>1</sub>).
+
+![Main](Qn2/Main.png)
 
